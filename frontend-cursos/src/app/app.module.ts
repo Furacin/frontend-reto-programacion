633ab58ef37c52c3a335136a9c03278c5b564be3
Fran <manzanile@gmail.com>
2020-08-20 22:39:00 +0200

    Añadido botón de Nuevo Curso. Añadido Dialog de Nuevo Curso con sus elementos de angular material.

--- frontend-cursos/src/app/app.module.ts
+++ frontend-cursos/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent, CursoDialogComponent } from './home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CursoDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +32,17 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     MatToolbarModule,
     MatCardModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSelectModule
   ],
+  entryComponents: [HomeComponent, CursoDialogComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, CursoDialogComponent]
 })
 export class AppModule { }
--- frontend-cursos/src/app/home/home.component.ts
+++ frontend-cursos/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatTableDataSource } from '@angular/material/table';
+import {MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 export interface Curso {
   id: String,
@@ -14,27 +15,80 @@ export interface Curso {
   apellidosProfesor: String 
 }
 
+export interface Profesor {
+  value: string;
+  viewValue: string;
+}
+
+export interface Nivel {
+  value: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  displayedColumns: string[] = ['titulo', 'nivel', 'numHoras', 'nombreProfesor'];
+  displayedColumns: string[] = ['titulo', 'nombreProfesor', 'nivel', 'numHoras'];
   listaCursos: Curso[];
   dataSource;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     // Simple GET request with response type <any>
     this.http.get<any>('http://localhost:8080/springboot-mybatis/api/becas/allCursos').subscribe(data => {
-      this.listaCursos  = data;
+      this.listaCursos = data;
       this.dataSource = new MatTableDataSource<Curso>(this.listaCursos);
       this.dataSource.paginator = this.paginator;
     })
+  }
+
+  nuevoCursoDialog() {
+    const dialogRef = this.dialog.open(CursoDialogComponent, {
+      width: '500px',
+      height: '500px',
+      //data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'nuevo-curso-dialog',
+  templateUrl: 'nuevo-curso-dialog.html',
+  styleUrls: ['./home.component.css']
+})
+export class CursoDialogComponent {
+  valor1="";
+  profesores: Profesor[] = [
+    {value: '1', viewValue: 'Roberto Canales'},
+    {value: '2', viewValue: 'David Gómez'},
+    {value: '3', viewValue: 'Alberto Moratilla'}
+  ];
+  niveles: Nivel[] = [
+    {value: 'Básico'},
+    {value: 'Intermedio'},
+    {value: 'Avanzado'}
+  ];
+
+  constructor(public dialogRef: MatDialogRef<CursoDialogComponent>) {}
+    //@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  close() {
+    this.dialogRef.close("Thanks for using me!");
   }
 
 }
